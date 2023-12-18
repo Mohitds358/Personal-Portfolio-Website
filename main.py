9e48660521684738542ccb7e56fea871fbd48f87
Mohit <mohithotwanids@gmail.com>
2023-12-18 19:36:43 +0530

    Add description on the new columns

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ col1, col2 = st.columns(2)
 with col1:
     st.image("images/photo.png")
 
+
 with col2:
     st.title("Ardit Sulce")
     content = """
     Lalana Dim kana
     """
     st.info(content)
+
+content2 = """
+Below  you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
